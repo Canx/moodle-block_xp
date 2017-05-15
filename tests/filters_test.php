9813e82861c69f45d507cf3343533aef78a32570
Ruben Cancho <canchete@gmail.com>
2017-05-15 10:36:48 +0200

    Forgot removing @group here.

--- tests/filters_test.php
+++ tests/filters_test.php
@@ -31,7 +31,6 @@ require_once($CFG->dirroot . '/blocks/xp/tests/fixtures/events.php');
  * Filters testcase.
  *
  * @package    block_xp
- * @group      block_xp
  * @copyright  2014 Frédéric Massart - FMCorz.net
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
